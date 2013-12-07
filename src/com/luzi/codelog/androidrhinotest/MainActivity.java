c24bbcbfc5d68ae9cc05905b724bbcc8fd5b425f
Luzi82 <luzi82@gmail.com>
2013-12-07 20:40:55 +0800

    sample code

--- src/com/luzi/codelog/androidrhinotest/MainActivity.java
+++ src/com/luzi/codelog/androidrhinotest/MainActivity.java
@@ -1,7 +1,12 @@
 package com.luzi.codelog.androidrhinotest;
 
-import android.os.Bundle;
+import org.mozilla.javascript.Callable;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +15,10 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		good0();
+		// bad0();
+		// bad1();
 	}
 
 	@Override
@@ -17,6 +26,35 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void good0() {
+		Context cx = Context.enter();
+		cx.setOptimizationLevel(-1);
+		Scriptable scope = cx.initStandardObjects();
+		cx.evaluateString(scope, "x=42;", "<cmd>", 0, null);
+		Object result = cx.evaluateString(scope, "x", "<cmd>", 0, null);
+		Log.v("androidrhinotest", result.toString());
+		Context.exit();
+	}
+
+	public void bad0() {
+		Context cx = Context.enter();
+		// cx.setOptimizationLevel(-1);
+		Scriptable scope = cx.initStandardObjects();
+		cx.evaluateString(scope, "x=42;", "<cmd>", 0, null);
+		cx.evaluateString(scope, "x", "<cmd>", 0, null); // die
+		Context.exit();
+	}
+
+	public void bad1() {
+		Context cx = Context.enter();
+		cx.setOptimizationLevel(-1);
+		Scriptable scope = cx.initStandardObjects();
+		cx.evaluateString(scope, "x=function(){return 42;};", "<cmd>", 0, null);
+		Callable c = (Callable) cx.evaluateString(scope, "x", "<cmd>", 0, null);
+		c.call(cx, scope, null, null); // die
+		Context.exit();
 	}
 
 }
